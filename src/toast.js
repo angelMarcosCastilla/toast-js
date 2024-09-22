f4cad6d92b4bac05329e411b53f984124077877c
angelMarcosCastilla <ajmarcosc15@gmail.com>
2024-09-22 17:57:53 -0500

    fix: generate id to toast and remove toast

--- src/toast.js
+++ src/toast.js
@@ -11,6 +11,10 @@ class Toast {
     this.toasts = [];
   }
 
+  generateId() {
+    return crypto.randomUUID();
+  }
+
   createContainer() {
     const container = document.createElement("div");
     container.className = "toast-container";
@@ -29,13 +33,10 @@ class Toast {
   }
 
   createToast(message, type = "default") {
-    if (this.toasts.length >= this.options.maxToasts ) {
-      this.removeToast(this.toasts[0]);
-      this.toasts.shift();
-    }
-
     const toast = document.createElement("div");
+    const currentToastId = this.generateId();
     toast.dataset.variant = type;
+    toast.dataset.toastId = currentToastId;
     toast.className = `toast`;
     toast.innerHTML = `
       <div class="toast-content">
@@ -43,18 +44,45 @@ class Toast {
       </div>
     `;
 
-    this.container.appendChild(toast);
-    this.toasts.push(toast);
+    if (this.options.closeButton) {
+      let closeButton = document.createElement("button");
+      closeButton.className = "toast-close-button";
+      closeButton.innerHTML = "❌";
+      toast.appendChild(closeButton);
+      closeButton.addEventListener(
+        "click",
+        () => {
+          this.removeToast(currentToastId);
+        },
+        { once: true }
+      );
+    }
 
-    setTimeout(() => {
-      this.removeToast(toast);
+    this.container.appendChild(toast);
+    this.toasts.push(currentToastId);
+    let timer = setTimeout(() => {
+      const hasId = this.toasts.includes(currentToastId);
+      if (hasId) {
+        this.removeToast(currentToastId);
+      } else {
+        clearTimeout(timer);
+      }
     }, this.options.duration);
+    if (this.toasts.length > this.options.maxToasts) {
+      const id = this.toasts[0];
+      this.removeToast(id);
+    }
 
     return toast;
   }
 
-  removeToast($toast) {
-    $toast.remove();
+  removeToast(toastId) {
+    let index = this.toasts.indexOf(toastId);
+    if (index !== -1) {
+      const $toast = document.querySelector(`[data-toast-id="${toastId}"]`);
+      $toast.remove();
+      this.toasts.splice(index, 1);
+    }
   }
 
   success(message) {
